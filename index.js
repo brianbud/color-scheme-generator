a5a8c1df6fd7f675b131003a207c04073965f386
Brian Bud <hello@brianbud.com>
2024-01-09 20:29:58 -0500

    get colors array

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const btn = document.querySelector('button');
 btn.addEventListener('click', getScheme);
+let colorsArray = [];
 
 function getScheme() {
   const colorPicker = document.querySelector('input');
   let hexCode = colorPicker.value.slice(1);
-  const modeChoice = document.querySelector('option').value;
+  const modeChoice = document.querySelector('#mode').value;
+  let apiData = [];
 
   fetch(
     `https://www.thecolorapi.com/scheme?hex=${hexCode}&mode=${modeChoice}&count=5`,
@@ -13,6 +15,10 @@ function getScheme() {
     }
   )
     .then((res) => res.json())
-    .then((data) => console.log(data));
-  console.log('clicked!', colorPicker.value.slice(1));
+    .then((data) => {
+      apiData.push(data);
+      colorsArray = apiData[0].colors;
+      console.log(apiData);
+      console.log(colorsArray);
+    });
 }
