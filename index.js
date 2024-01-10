424a69d2c95fa670ae102a45ed6f0cf965b1d884
Brian Bud <hello@brianbud.com>
2024-01-09 20:02:01 -0500

    fetch api

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const btn = document.querySelector('button');
-const colorPicker = document.querySelector('input');
-btn.addEventListener('click', (e) => {
-  e.preventDefault();
-  console.log('clicked!', colorPicker.value);
-});
+btn.addEventListener('click', getScheme);
+
+function getScheme() {
+  const colorPicker = document.querySelector('input');
+  let hexCode = colorPicker.value.slice(1);
+
+  fetch(
+    `https://www.thecolorapi.com/scheme?hex=${hexCode}&mode=analogic&count=5`,
+    {
+      method: 'GET',
+    }
+  )
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+  console.log('clicked!', colorPicker.value.slice(1));
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+API practice using The Color API: https://www.thecolorapi.com/docs
